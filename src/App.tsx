cf1530e5584d295c372a75a0ee6868ee756542c0
kyu3638 <kyu3638@gmail.com>
2024-01-25 15:49:32 +0900

    Feat:login,signup  router

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,23 @@
 import './App.css';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Login from './pages/Login';
+import SignUp from './pages/SignUp';
+import Home from './pages/Home';
+import NavBar from './layout';
 
 function App() {
-  return <div>deploy completed</div>;
+  return (
+    <>
+      <BrowserRouter>
+        <NavBar />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<SignUp />} />
+        </Routes>
+      </BrowserRouter>
+    </>
+  );
 }
 
 export default App;
