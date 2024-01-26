84b66068e45548029b8c5abe34c94bf9ff743665
kyu3638 <kyu3638@gmail.com>
2024-01-26 16:04:45 +0900

    Feat:이미지 업로드 및 storage이미지 경로를 user DB에 갱신

--- src/pages/SignUpStepTwo/index.tsx
+++ src/pages/SignUpStepTwo/index.tsx
@@ -2,18 +2,23 @@ import { useUserUid } from '@/contexts/LoginUserState';
 import { doc, updateDoc } from 'firebase/firestore';
 import { InputsForm, LoginInput } from '../Login';
 import { useEffect, useState } from 'react';
-import { db } from '@/firebase/firebase';
+import { db, storage } from '@/firebase/firebase';
 import { useNavigate } from 'react-router-dom';
-// import { IUser } from '@/types/common';
+import { getDownloadURL, ref, uploadBytes } from 'firebase/storage';
 
 const SignUpStepTwo = () => {
   const [nickName, setNickName] = useState('');
   const [introduction, setIntroduction] = useState('');
+  const [selectedFile, setSelectedFile] = useState<File | null>(null);
+  const [imageURL, setImageURL] = useState('');
   const { userUid, isLogin } = useUserUid();
+
+  const navigate = useNavigate();
+
   useEffect(() => {
     console.log(`2단계 페이지에서 로그인 여부 : `, isLogin);
+    console.log(`userUid : ${userUid}`);
   }, []);
-  const navigate = useNavigate();
 
   const onChangeNickName = (e: React.ChangeEvent<HTMLInputElement>) => {
     setNickName(e.target.value);
@@ -22,28 +27,48 @@ const SignUpStepTwo = () => {
     setIntroduction(e.target.value);
   };
 
-  const signUpHandler = async () => {
-    // 작성한 것들이 유효한지 확인
+  const onHandleFileSelect = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const file = e.target.files ? e.target.files[0] : null;
+    setSelectedFile(file);
+  };
 
-    // db업데이트
-    const userUpdate = async () => {
-      try {
-        const newData = { nickName: nickName, introduction: introduction, updatedAt: new Date() };
-        const docRef = doc(db, 'users', userUid);
-        await updateDoc(docRef, newData).then(() => {
-          console.log(`유저 정보가 업데이트 되었습니다.`);
-          navigate('/');
-        });
-      } catch (error) {
-        console.log(error);
-      }
-    };
-    userUpdate();
+  const handleUpload = async (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    console.log(e);
+    e.preventDefault();
+    if (selectedFile) {
+      const imageRef = ref(storage, `userImage/${userUid}/${selectedFile.name}`);
+      await uploadBytes(imageRef, selectedFile);
+      // storage에서 파일의 url
+      const downloadURL = await getDownloadURL(imageRef);
+      setImageURL(downloadURL);
+    }
+  };
+
+  const signUpHandler = async () => {
+    try {
+      const newData = {
+        nickName: nickName,
+        introduction: introduction,
+        profileImage: imageURL,
+        updatedAt: new Date(),
+      };
+      console.log(userUid);
+      const docRef = doc(db, 'users', userUid);
+      console.log(docRef);
+      await updateDoc(docRef, newData).then(() => {
+        console.log(`유저 정보가 업데이트 되었습니다.`);
+        navigate('/');
+      });
+    } catch (error) {
+      console.log(error);
+    }
   };
 
   return (
     <div className="h-lvh flex flex-col justify-center items-center">
       <InputsForm>
+        <input type="file" onChange={onHandleFileSelect} />
+        <button onClick={handleUpload}>Upload</button>
         <LoginInput
           label={'닉네임'}
           type={'text'}
