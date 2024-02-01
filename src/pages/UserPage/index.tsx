9b594825eee4fa514c7186b8d58ce8c99f1bb2d7
kyu3638 <kyu3638@gmail.com>
2024-02-01 20:46:18 +0900

    Refactor:기존의 API통신 TanstackQuery로 리팩토링

--- src/pages/UserPage/index.tsx
+++ src/pages/UserPage/index.tsx
@@ -1,35 +1,49 @@
-import { IUser } from '@/types/common';
-import { useEffect, useState } from 'react';
+// import { useEffect, useState } from 'react';
 import { db } from '@/firebase/firebase';
 import { doc, getDoc } from 'firebase/firestore';
 import PageWrap from '@/components/Wrap/PageWrap';
 import Profile from '@/pages/MyPage/Profile';
 import { useParams } from 'react-router-dom';
+import { useQuery } from '@tanstack/react-query';
+import { IUser } from '@/types/common';
 
 const UserPage = () => {
-  const [user, setUser] = useState<IUser | null>(null);
+  // const [user, setUser] = useState<IUser | null>(null);
   const { userUid } = useParams();
   console.log(`불러올 유저의 uid : `, userUid);
   console.log(useParams());
 
-  /** db에서 유저를 불러옴 */
-  useEffect(() => {
-    // 렌더링 될 때 유저의 정보가 뿌려지도록..
-    const getUser = async () => {
-      const userRef = doc(db, 'users', userUid as string);
-      await getDoc(userRef)
-        .then((res) => {
-          const userData = JSON.stringify(res.data());
-          setUser(JSON.parse(userData));
-          return JSON.parse(userData);
-        })
-        .then((user) => {
-          console.log(`현재 조회된 유저 : `, user);
-        });
-    };
-    getUser();
-  }, []);
+  // /** db에서 유저를 불러옴 */
+  // useEffect(() => {
+  //   // 렌더링 될 때 유저의 정보가 뿌려지도록..
+  //   const getUser = async () => {
+  //     const userRef = doc(db, 'users', userUid as string);
+  //     await getDoc(userRef)
+  //       .then((res) => {
+  //         const userData = JSON.stringify(res.data());
+  //         setUser(JSON.parse(userData));
+  //         return JSON.parse(userData);
+  //       })
+  //       .then((user) => {
+  //         console.log(`현재 조회된 유저 : `, user);
+  //       });
+  //   };
 
+  //   getUser();
+  // }, []);
+
+  const fetchUser = async (): Promise<IUser | undefined> => {
+    try {
+      const res = await getDoc(doc(db, 'users', userUid as string));
+      console.log(res);
+      console.log(res.data());
+      const user = res.data() as IUser;
+      return user;
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  const { data: user } = useQuery({ queryKey: ['user'], queryFn: fetchUser, refetchOnWindowFocus: true });
   return (
     <PageWrap>
       <Profile user={user!} />
