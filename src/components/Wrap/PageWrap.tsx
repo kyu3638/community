5f1b64a8c3cd448ce46a6392dbb0767df5c4c596
kyu3638 <kyu3638@gmail.com>
2024-02-03 20:17:53 +0900

    Feat:Froala Editor set

--- src/components/Wrap/PageWrap.tsx
+++ src/components/Wrap/PageWrap.tsx
@@ -1,11 +1,7 @@
 import { IChildrenProps } from '@/types/common';
 
 const PageWrap = ({ children }: IChildrenProps) => {
-  return (
-    <div className="flex justify-center items-center">
-     {children}
-    </div>
-  );
+  return <div className="flex justify-center gap-10">{children}</div>;
 };
 
 export default PageWrap;
--- src/pages/Posting/index.tsx
+++ src/pages/Posting/index.tsx
@@ -1,7 +1,34 @@
 import PageWrap from '@/components/Wrap/PageWrap';
+import 'froala-editor/css/froala_style.min.css';
+import 'froala-editor/css/froala_editor.pkgd.min.css';
+
+import FroalaEditorComponent from 'react-froala-wysiwyg';
+import EditorWrap from '@/components/Wrap/EditorWrap';
+import FroalaEditorView from 'react-froala-wysiwyg/FroalaEditorView';
+import { useState } from 'react';
 
 const Posting = () => {
-  return <PageWrap>asdf</PageWrap>;
+  const [model, setModel] = useState('Example Set');
+  console.log(model);
+
+  const config = {
+    placeholderText: '게시글을 여기에 작성해주세요',
+  };
+
+  const handleModelChange = (event: any) => {
+    setModel(event);
+  };
+
+  return (
+    <PageWrap>
+      <EditorWrap>
+        <FroalaEditorComponent tag="textarea" config={config} onModelChange={handleModelChange} />
+      </EditorWrap>
+      <EditorWrap>
+        <FroalaEditorView model={model} />
+      </EditorWrap>
+    </PageWrap>
+  );
 };
 
 export default Posting;
