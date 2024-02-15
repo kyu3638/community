90fbce130c337b912df3f14006277a69d4867ebe
kyu3638 <kyu3638@gmail.com>
2024-02-16 04:33:05 +0900

    Refactor:유저조회 쿼리 로직 개선

---
+++ src/apis/user/users.ts
@@ -0,0 +1,59 @@
+import { db } from '@/firebase/firebase';
+import {
+  collection,
+  orderBy,
+  query,
+  startAfter,
+  limit,
+  getDocs,
+  doc,
+  updateDoc,
+  arrayUnion,
+  arrayRemove,
+} from 'firebase/firestore';
+
+export const fetchUsers = async ({ queryKey, pageParam }) => {
+  console.log(pageParam);
+  try {
+    const keyword = queryKey[1];
+    // 검색 키워드가 있는 경우와 없는 경우 구분
+    if (!keyword) {
+      const usersRef = collection(db, 'users');
+      let q;
+      if (pageParam) {
+        console.log(`pageParam`, pageParam);
+        q = query(usersRef, orderBy('createdAt'), startAfter(pageParam), limit(5));
+      } else {
+        q = query(usersRef, orderBy('createdAt'), limit(5));
+      }
+
+      const result = (await getDocs(q)).docs;
+      const querySnapshot = result.map((doc) => doc.data());
+      const lastDoc = result[result.length - 1];
+      return { querySnapshot, lastDoc };
+    } else {
+      const usersRef = collection(db, 'users');
+      const querySnapshot = (await getDocs(usersRef)).docs
+        .map((doc) => doc.data())
+        .filter((userData) => userData.nickName.includes(keyword));
+
+      return { querySnapshot, lastDoc: null };
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+
+export const followHandler = async ({ userUid, targetUid, type }) => {
+  try {
+    const command = type === 'addFollowing' ? arrayUnion : arrayRemove;
+
+    const myDocRef = doc(db, 'users', userUid!);
+    await updateDoc(myDocRef, { following: command(targetUid) });
+
+    const targetDocRef = doc(db, 'users', targetUid);
+    await updateDoc(targetDocRef, { follower: command(userUid) });
+  } catch (error) {
+    console.log(error);
+  }
+};
