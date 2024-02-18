a475391299ff2e94a28b7a80205d8199b5825b8b
kyu3638 <kyu3638@gmail.com>
2024-02-19 01:45:25 +0900

    Design:게시글 UI를 유저 카드 UI와 같은 스타일로 변경

--- src/components/Wrap/ArticleWrap.tsx
+++ src/components/Wrap/ArticleWrap.tsx
@@ -1,7 +1,7 @@
 import { IChildrenProps } from '@/types/common';
 
 const ArticleWrap = ({ children }: IChildrenProps) => {
-  return <div className="flex flex-col gap-5 border border-indigo-500 bg-indigo-50 shadow-lg p-5">{children}</div>;
+  return <div className="flex flex-col gap-5 border-t border-b border-black bg-gray-100 shadow-lg p-5">{children}</div>;
 };
 
 export default ArticleWrap;
