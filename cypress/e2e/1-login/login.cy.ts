56853aca77353eaa0a022dcfc49918d10966c0c0
kyu3638 <kyu3638@gmail.com>
2024-02-20 13:16:13 +0900

    Comment:로그인 성공 시나리오 테스트 케이스 주석 입력

--- cypress/e2e/1-login/login.cy.ts
+++ cypress/e2e/1-login/login.cy.ts
@@ -1,8 +1,12 @@
-describe('template spec', () => {
-  it('visit', () => {
+describe('로그인 화면', () => {
+  it('사용자는 아이디와 비밀번호를 사용해서 로그인한다.', () => {
+    // given - 로그인 화면에 접근한다.
     cy.visit('/login');
+
     cy.get('[data-cy=idInput]').as('idInput');
     cy.get('[data-cy=passwordInput]').as('passwordInput');
+
+    // when - 아이디와 비밀번호를 입력하고 로그인 버튼을 클릭한다.
 
     cy.get('@idInput').type('tmdrbwjs@naver.com');
     cy.get('@passwordInput').type('asdfasdf!@');
@@ -13,7 +17,7 @@ describe('template spec', () => {
     cy.get('[data-cy=loginButton]').as('loginButton');
     cy.get('[data-cy=loginButton]').click();
 
+    // then - 로그인에 성공하고 메인화면으로 이동한다.
     cy.url().should('include', 'http://localhost:5173/');
   });
 });
- 
